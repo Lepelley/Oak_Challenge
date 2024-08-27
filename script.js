91874491d2d0cca393f9ab90689b56bd08bf7df6
Vincent Lepelley <vincent@lepelley.fr>
2024-08-27 16:45:09 +0200

    0.2
    - localStorage
    - catched
    - pre-badge 2 Blue

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const table = document.querySelector('#js-table')
 let index = 0
+let pokemonData = null
+if (window.localStorage.getItem(name) !== null) {
+  pokemonData = JSON.parse(window.localStorage.getItem(name))
+}
 
 pokemon.forEach((poke, index) => {
   const imgElement = document.createElement('img')
-  imgElement.src = poke
+  imgElement.src = poke.image
+  if (poke.caught || (pokemonData && pokemonData[index] && pokemonData[index].caught)) {
+    imgElement.classList.add('caught')
+  }
 
   imgElement.addEventListener('click', () => {
     imgElement.classList.toggle('caught')
+    poke.caught = imgElement.classList.contains('caught')
+
     changeCounter()
+    saveData()
   })
   table.appendChild(imgElement)
 })
+
+const saveData = () => {
+  window.localStorage.setItem(name, JSON.stringify(pokemon))
+}
 
 const changeCounter = () => {
   const nbCaught = document.querySelectorAll('.caught').length
